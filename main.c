c1f198f6aff40824518d00e2c645ec99a3e0c3cb
Northmatrix <contact@northmatrix.co.uk>
2025-04-25 04:24:32 +0100

    i am not sure what way

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <SDL3/SDL_events.h>
 #define SDL_MAIN_USE_CALLBACKS 1
 
 #include <SDL3/SDL.h>
@@ -13,44 +14,52 @@
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 640
-#define PIXEL 32
-#define PIXEL_P_WIDTH SCREEN_WIDTH / PIXEL
-#define PIXEL_P_HEIGHT SCREEN_HEIGHT / PIXEL
+#define UNIT 32
+#define UNIT_WDITH SCREEN_WIDTH / UNIT
+#define UNIT_HEIGHT SCREEN_HEIGHT / UNIT
+
+typedef struct {
+  float x;
+  float y;
+  int id;
+} Tile;
+
+typedef struct {
+  Tile grid[UNIT_HEIGHT][256];
+} TileMap;
 
 typedef struct {
   SDL_Window *window;
   SDL_Renderer *renderer;
+  TileMap tilemap;
   uint64_t last_step;
 } AppState;
 
-typedef struct {
-  int grid[PIXEL_P_HEIGHT][PIXEL_P_WIDTH];
-} Map;
-
-void map_init(Map *map) {
-  for (int y = 0; y < PIXEL_P_HEIGHT; y++) {
-    for (int x = 0; x < PIXEL_P_WIDTH; x++) {
-      map->grid[y][x] = 0;
+void map_init(TileMap *map) {
+  for (int y = 0; y < UNIT_HEIGHT; y++) {
+    for (int x = 0; x < UNIT_WDITH; x++) {
+      Tile tile = {x * UNIT, y * UNIT, 0};
+      map->grid[y][x] = tile;
     }
   }
 }
 
-int load_map_from_csv(const char *filename, Map *map) {
+int load_map(const char *filename, TileMap *map) {
   FILE *file = fopen(filename, "r");
   if (!file) {
     perror("Could not open file");
-    return 0;
+    return 1;
   }
 
-  char line[512]; // Increase buffer for wide maps
+  char line[512];
   int y = 0;
 
-  while (fgets(line, sizeof(line), file) && y < PIXEL_P_HEIGHT) {
+  while (fgets(line, sizeof(line), file) && y < UNIT_HEIGHT) {
     int x = 0;
     char *token = strtok(line, ",");
 
-    while (token && x < PIXEL_P_WIDTH) {
-      map->grid[y][x] = atoi(token);
+    while (token && x < UNIT_WDITH) {
+      map->grid[y][x].id = atoi(token);
       token = strtok(NULL, ",");
       x++;
     }
@@ -59,20 +68,26 @@ int load_map_from_csv(const char *filename, Map *map) {
   }
 
   fclose(file);
-  return 1;
+  return 0;
 }
 
-void draw_map(Map *map, SDL_Renderer *renderer) {
-  for (int y = 0; y < PIXEL_P_HEIGHT; y++) {
-    for (int x = 0; x < PIXEL_P_WIDTH; x++) {
-      if (map->grid[y][x] == 1) {
-        SDL_SetRenderDrawColor(renderer, 255, 25, 25, 255); // RED
-        SDL_FRect square = {x * PIXEL, y * PIXEL, PIXEL,
-                            PIXEL}; // x, y, width, height
+void draw_map(TileMap *map, SDL_Renderer *renderer) {
+  for (int y = 0; y < UNIT_HEIGHT; y++) {
+    for (int x = 0; x < UNIT_WDITH; x++) {
+      Tile tile = map->grid[y][x];
+      if (tile.id == 1) {
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_FRect square = {tile.x, tile.y, UNIT, UNIT};
         SDL_RenderFillRect(renderer, &square);
       }
     }
   }
+}
+
+void draw_player(float x, float y, SDL_Renderer *renderer) {
+  SDL_SetRenderDrawColor(renderer, 25, 25, 255, 255);
+  SDL_FRect square = {x, y, UNIT, UNIT * 2}; // x, y, width, height
+  SDL_RenderFillRect(renderer, &square);
 }
 
 int LoadAndSetBMP(SDL_Window *window) {
@@ -85,6 +100,28 @@ int LoadAndSetBMP(SDL_Window *window) {
   SDL_DestroySurface(icon);
   icon = NULL;
   return 0;
+}
+
+SDL_AppResult handle_key_event(TileMap *tilemap, SDL_Scancode key_code) {
+  if (key_code == SDL_SCANCODE_W) {
+  }
+  if (key_code == SDL_SCANCODE_A) {
+    for (int y = 0; y < UNIT_HEIGHT; y++) {
+      for (int x = 0; x < UNIT_WDITH; x++) {
+        tilemap->grid[y][x].x -= 16.0f;
+      }
+    }
+  }
+  if (key_code == SDL_SCANCODE_S) {
+  }
+  if (key_code == SDL_SCANCODE_D) {
+    for (int y = 0; y < UNIT_HEIGHT; y++) {
+      for (int x = 0; x < UNIT_WDITH; x++) {
+        tilemap->grid[y][x].x += 16.0f;
+      }
+    }
+  }
+  return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
@@ -112,22 +149,33 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   as->last_step = SDL_GetTicks();
   LoadAndSetBMP(as->window);
 
-  Map map;
-  map_init(&map);
-  load_map_from_csv("map.csv", &map);
-  draw_map(&map, as->renderer);
-  printf("%d", PIXEL_P_HEIGHT);
-  printf("%d", PIXEL_P_WIDTH);
+  // load and set the tilemap
+  map_init(&as->tilemap);
+  load_map("map.csv", &as->tilemap);
+
+  SDL_SetRenderDrawColor(as->renderer, 0, 0, 0, 255);
+  SDL_RenderClear(as->renderer);
+  SDL_RenderPresent(as->renderer);
+
+  return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+  AppState *as = appstate;
+  SDL_SetRenderDrawColor(as->renderer, 0, 0, 0, 255);
+  SDL_RenderClear(as->renderer);
+  draw_map(&as->tilemap, as->renderer);
   SDL_RenderPresent(as->renderer);
   return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppIterate(void *appstate) { return SDL_APP_CONTINUE; }
-
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+  AppState *as = appstate;
   switch (event->type) {
   case SDL_EVENT_QUIT:
     return SDL_APP_SUCCESS;
+  case SDL_EVENT_KEY_DOWN:
+    return handle_key_event(&as->tilemap, event->key.scancode);
   }
   return SDL_APP_CONTINUE;
 }
