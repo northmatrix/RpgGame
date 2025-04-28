e6dd9cf613873ea9180ca5f106b0d0b31636fe16
Northmatrix <contact@northmatrix.co.uk>
2025-04-28 03:41:30 +0100

    before going crazy

--- input.c
+++ input.c
@@ -3,16 +3,16 @@
 SDL_AppResult handle_key_event(Camera *camera, float deltatime, int **tile,
                                SDL_Scancode key_code) {
   if (key_code == SDL_SCANCODE_W) {
-    camera->y -= SPEED * deltatime;
-  }
-  if (key_code == SDL_SCANCODE_A) {
-    camera->x -= SPEED * deltatime;
-  }
-  if (key_code == SDL_SCANCODE_S) {
     camera->y += SPEED * deltatime;
   }
-  if (key_code == SDL_SCANCODE_D) {
+  if (key_code == SDL_SCANCODE_A) {
     camera->x += SPEED * deltatime;
+  }
+  if (key_code == SDL_SCANCODE_S) {
+    camera->y -= SPEED * deltatime;
+  }
+  if (key_code == SDL_SCANCODE_D) {
+    camera->x -= SPEED * deltatime;
   }
   return SDL_APP_CONTINUE;
 }
